f1be328e8ab19fbc93014473a5818ac061ff97a3
Yujin <qrt365@naver.com>
2023-03-02 22:00:00 +0900

    23.03.02

---
+++ src/BJ_23_03/BJ_24263.java
@@ -0,0 +1,14 @@
+package BJ_23_03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Integer.parseInt(br.readLine()));
+        System.out.println("1");
+    }
+}
