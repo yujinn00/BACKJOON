ade99e02fa88c322cb2737a8fec4fa5319207d63
Yujin <qrt365@naver.com>
2023-03-05 00:49:12 +0900

    23.03.05

---
+++ src/BJ_23_03/BJ_24266.java
@@ -0,0 +1,16 @@
+package BJ_23_03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_24266 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println(n * n * n);
+        System.out.println("3");
+    }
+}
