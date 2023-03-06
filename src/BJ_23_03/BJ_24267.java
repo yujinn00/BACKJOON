7ff752ccc1d176da1353d87bf96be5b021997e15
Yujin <qrt365@naver.com>
2023-03-07 02:07:13 +0900

    23.03.07

--- src/BJ_23_03/BJ_24267.java
+++ src/BJ_23_03/BJ_24267.java
@@ -1,7 +1,27 @@
 package BJ_23_03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BJ_24267 {
-    public static void main(String[] args) {
-        System.out.println("HI");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+        long sum = 0;
+
+        if (n == 1 || n == 2) {
+            System.out.println("0");
+            System.out.println("3");
+
+        } else {
+            for (int i = 1; i <= n - 2; i++) {
+                sum += ((long) i * (i + 1) / 2);
+            }
+
+            System.out.println(sum);
+            System.out.println("3");
+        }
     }
 }
